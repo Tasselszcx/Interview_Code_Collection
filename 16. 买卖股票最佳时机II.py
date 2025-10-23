f8b2560a6dc1d3b2535b4c20ded1aacc1578cf80
张宸旭 <3199601798@qq.com>
2025-10-23 23:30:24 +0100

    贪心

--- 16. 买卖股票最佳时机II.py
+++ 16. 买卖股票最佳时机II.py
@@ -30,11 +30,14 @@ def max_profit_greedy(prices):
     """
     if not prices or len(prices) < 2:
         return 0
+    
     max_profit = 0
+    
     for i in range(1, len(prices)):
+        # 如果今天价格比昨天高，就累加利润
         if prices[i] > prices[i - 1]:
             max_profit += prices[i] - prices[i - 1]
-        
+    
     return max_profit
 
 def test_max_profit_ii():
